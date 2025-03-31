77c9821a268a3ee63955f88aa124ce1b34cf9b27
ChowChowSonic <joeyant1367@gmail.com>
2025-03-31 14:42:10 -0400

    formatting fixes

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-import sys 
-import os 
-import qrcode 
-import time 
-from dotenv import load_dotenv 
-import logging.config 
-load_dotenv() 
+"""Main function and driver code"""
+import sys
+import os
+import qrcode
+import time
+from dotenv import load_dotenv
+import logging.config
+load_dotenv()
 
-def main(data): 
+def main(data: [str]):
+	"""Main function and driver code"""
 	try: 
 		dir = os.getenv("QR_DIR", "./qr-codes")
 		logging.basicConfig(
-			level=logging.INFO,
+			level=logging.DEBUG,
 			format='%(asctime)s - %(levelname)s - %(message)s',
 			handlers=[
             logging.StreamHandler(sys.stdout),
-        	]
+			logging.FileHandler('app.log', mode="a"),
+        	], 
     	)
+		# Ensure that the handlers are properly configured
+		logging.info("Application started successfully")
 		os.makedirs(dir, exist_ok=True)
 		code = qrcode.QRCode(version=3, box_size=10, border=1)
 		code.add_data(data[1])
@@ -29,7 +34,6 @@ def main(data):
 		logging.error(f"Failed to create directory {dir}")
 	except IndexError: 
 		logging.error("Please provide a url as the first argument when running this program")
-
 
 if __name__ == "__main__":
 	main(sys.argv) #pragma: no cov 
